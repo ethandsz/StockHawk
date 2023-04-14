cd8cad762721dac9075dc3f2e6b1d057c2b06abd
Ethan D'Souza <91471122+ethandsz@users.noreply.github.com>
2023-04-15 00:49:04 +0100

    added new holdlevel type

---
+++ StockHawk/ComputationalLayer/Models/HoldLevel.cs
@@ -0,0 +1,8 @@
+﻿namespace StockHawk.ComputationalLayer.Models;
+
+public class HoldLevel
+{
+    public bool IsInverse { get; set; }
+    public double Level { get; set; }
+    public Int64 TimeStamp { get; set; }
+}
