79947bb79fc5db7ee6e2a8595c395bfccb62475d
Ethan D'Souza <91471122+ethandsz@users.noreply.github.com>
2023-04-15 00:49:41 +0100

    Implemented new methods

--- StockHawk/Program.cs
+++ StockHawk/Program.cs
@@ -1,15 +1,12 @@
 ﻿using StockHawk.Api;
-using StockHawk.Api.Models;
 using StockHawk.ComputationalLayer;
 
 var httpRequester = HttpRequester.Instance();
 var holdLevelFinder = HoldLevelFinder.Instance();
-var endpoint = new Uri("https://api.bybit.com/spot/v3/public/quote/kline?symbol=BTCUSDT&interval=1h&limit=25");
-try 
+try
 {
-    var result = await httpRequester.GetFromJsonAsync<ByBitResponse>(endpoint);
-    var wicks = result.Result.Wicks;
-    holdLevelFinder.IdentifyWickRanges(wicks!);
+    var result = await httpRequester.RequestSticksWithLimit("1m", "60");
+    var levels = await holdLevelFinder.GetLevels(result);
     Console.WriteLine("Ry");
 } 
 catch (Exception ex)
