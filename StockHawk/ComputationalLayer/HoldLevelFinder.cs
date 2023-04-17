bfd15efa1788dd91a72716bcf7a9de048ea7668f
Ethan D'Souza <91471122+ethandsz@users.noreply.github.com>
2023-04-17 22:20:02 +0100

    Faster method to find untested levels

--- StockHawk/ComputationalLayer/HoldLevelFinder.cs
+++ StockHawk/ComputationalLayer/HoldLevelFinder.cs
@@ -28,9 +28,10 @@ public class HoldLevelFinder : IHoldLevelFinder
     public async Task<List<HoldLevel>> GetLevels(ByBitResponse response, string timeInterval)
     {
         var possibleLevels = IdentifyHighLevelHoldLevels(response.Result.Wicks);
-        var untestedHoldLevels = await GetUntestedLevels(possibleLevels);
+        var lowerLevels = await ConvertHighLevelHoldLevels(possibleLevels, timeInterval);
+       // var untestedHoldLevels = await GetUntestedLevels(possibleLevels);
 
-        return untestedHoldLevels;
+        return lowerLevels;
     }
 
     /// <summary>
@@ -75,27 +76,105 @@ public class HoldLevelFinder : IHoldLevelFinder
         return holdLevels;
     }
 
-    private async Task<List<HoldLevel>> GetUntestedLevels(List<HoldLevel> holdLevels)
+    private async Task<List<HoldLevel>> ConvertHighLevelHoldLevels(List<HoldLevel> highLevels, string timeInterval)
     {
-        var unTestedLevels = new List<HoldLevel>();
+        var milliSeconds = timeInterval.ConvertToMillis();
+        var holdLevels = new List<HoldLevel>();
         var currentTime = await _httpRequester.GetCurrentTime();
-        var numOfLevels = holdLevels.Count;
-        foreach (var holdLevel in holdLevels)
+        var numOfLevels = highLevels.Count;
+        foreach (var holdLevel in highLevels)
         {
             var startTime = holdLevel.TimeStamp;
             var sticks = new List<CandleStick>();
             while (startTime < currentTime)
             {
                 var requestedSticks =
-                    await _httpRequester.RequestSticksWithTimeLimit("1m", startTime.ToString(), (startTime + 60000000).ToString());
-                startTime += 60000000;
-                // var possibleSticksInRange = sticks.Where(w => w.IsBullish != holdLevel.IsInverse);
-                // if (holdLevel.IsInverse)
-                // {
-                //     var firstNonBullishClosedBody = possibleSticksInRange.Where(p => p.Open < holdLevel.Level).FirstOrDefault();
-                // }
+                    await _httpRequester.RequestSticksWithTimeLimit(timeInterval, startTime.ToString(),
+                        currentTime.ToString());
+                startTime += milliSeconds * 1000;
                 sticks.AddRange(requestedSticks.Result.Wicks);
             }
+            
+            var sticksToTestForInverseLevel = sticks.Where(s => s.High > holdLevel.Level && s.Low < holdLevel.Level && s.TimeStamp > holdLevel.TimeStamp).ToList();
+                    
+            if (!await IsTested(sticksToTestForInverseLevel, holdLevel, timeInterval))
+            {
+                holdLevels.Add(holdLevel);
+            }
+
+            numOfLevels--;
+            Console.WriteLine($"Progress: {100 * (double)(highLevels.Count - numOfLevels) / highLevels.Count}");
+        }
+        return holdLevels;
+    }
+
+    // private async Task<List<HoldLevel>> GetUntestedLevels(List<HoldLevel> holdLevels)
+    // {
+    //     var unTestedLevels = new List<HoldLevel>();
+    //     var currentTime = await _httpRequester.GetCurrentTime();
+    //     var numOfLevels = holdLevels.Count;
+    //     foreach (var holdLevel in holdLevels)
+    //     {
+    //         var startTime = holdLevel.TimeStamp;
+    //         var sticks = new List<CandleStick>();
+    //         while (startTime < currentTime)
+    //         {
+    //             var requestedSticks =
+    //                 await _httpRequester.RequestSticksWithTimeLimit("1m", startTime.ToString(), (startTime + 60000000).ToString());
+    //             startTime += 60000000;
+    //             // var possibleSticksInRange = sticks.Where(w => w.IsBullish != holdLevel.IsInverse);
+    //             // if (holdLevel.IsInverse)
+    //             // {
+    //             //     var firstNonBullishClosedBody = possibleSticksInRange.Where(p => p.Open < holdLevel.Level).FirstOrDefault();
+    //             // }
+    //             sticks.AddRange(requestedSticks.Result.Wicks);
+    //         }
+    //         var possibleSticks = sticks.Where(w => w.IsBullish != holdLevel.IsInverse);
+    //         if (holdLevel.IsInverse)
+    //         {
+    //             var firstNonBullishClosedBody = possibleSticks.Where(p => p.Open < holdLevel.Level).FirstOrDefault();
+    //             if (firstNonBullishClosedBody is not null)
+    //             {
+    //                 possibleSticks = sticks.Where(s => s.TimeStamp > firstNonBullishClosedBody.TimeStamp);
+    //                 possibleSticks = possibleSticks.Where(w => w.IsBullish == holdLevel.IsInverse);
+    //                 if (!possibleSticks.Any(p => p.High > holdLevel.Level * .9999))
+    //                 {
+    //                     unTestedLevels.Add(holdLevel);
+    //                 }
+    //             }
+    //             continue;
+    //         }
+    //         
+    //         var firstBullishClosedBody = possibleSticks.Where(p => p.Open > holdLevel.Level).FirstOrDefault();
+    //         if (firstBullishClosedBody is not null)
+    //         {
+    //             possibleSticks = sticks.Where(s => s.TimeStamp > firstBullishClosedBody.TimeStamp);
+    //             possibleSticks = possibleSticks.Where(w => w.IsBullish == holdLevel.IsInverse);
+    //             if (!possibleSticks.Any(p => p.Low < holdLevel.Level * 1.0001))
+    //             {
+    //                 unTestedLevels.Add(holdLevel);
+    //             }
+    //         }
+    //
+    //         numOfLevels--;
+    //         Console.WriteLine($"Progress: {(double)(holdLevels.Count - numOfLevels) / holdLevels.Count}");
+    //     }
+    //
+    //     return unTestedLevels;
+    // }
+    
+    private async Task<bool> IsTested(List<CandleStick> candleSticks, HoldLevel holdLevel, string timeInterval)
+    {
+        if (candleSticks.Count == 0)
+        {
+            return true;
+        }
+        await _httpRequester.GetCurrentTime();
+        foreach (var candleStick in candleSticks)
+        {
+            var response = await _httpRequester.RequestSticksWithTimeLimit("1m", candleStick.TimeStamp.ToString(),
+                (candleStick.TimeStamp + timeInterval.ConvertToMillis()).ToString());
+            var sticks = response.Result.Wicks;
             var possibleSticks = sticks.Where(w => w.IsBullish != holdLevel.IsInverse);
             if (holdLevel.IsInverse)
             {
@@ -104,9 +183,9 @@ public class HoldLevelFinder : IHoldLevelFinder
                 {
                     possibleSticks = sticks.Where(s => s.TimeStamp > firstNonBullishClosedBody.TimeStamp);
                     possibleSticks = possibleSticks.Where(w => w.IsBullish == holdLevel.IsInverse);
-                    if (!possibleSticks.Any(p => p.High > holdLevel.Level * .9999))
+                    if (possibleSticks.Any(p => p.High > holdLevel.Level * .9999))
                     {
-                        unTestedLevels.Add(holdLevel);
+                        return true;
                     }
                 }
                 continue;
@@ -117,16 +196,14 @@ public class HoldLevelFinder : IHoldLevelFinder
             {
                 possibleSticks = sticks.Where(s => s.TimeStamp > firstBullishClosedBody.TimeStamp);
                 possibleSticks = possibleSticks.Where(w => w.IsBullish == holdLevel.IsInverse);
-                if (!possibleSticks.Any(p => p.Low < holdLevel.Level * 1.0001))
+                if (possibleSticks.Any(p => p.Low < holdLevel.Level * 1.0001))
                 {
-                    unTestedLevels.Add(holdLevel);
+                    return true;
                 }
             }
-
-            numOfLevels--;
-            Console.WriteLine($"Progress: {(double)(holdLevels.Count - numOfLevels) / holdLevels.Count}");
         }
 
-        return unTestedLevels;
+        return false;
     }
+    
 }
